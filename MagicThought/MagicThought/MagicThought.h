61a1ae51d26c2b66abc3e5671c80fbeb5ef911c0
monda <monda@wycdeiMac.local>
2019-03-13 17:30:52 +0800

    no message

---
+++ MagicThought/MagicThought/MagicThought.h
@@ -0,0 +1,19 @@
+//
+//  MagicThought.h
+//  MagicThought
+//
+//  Created by monda on 2019/3/13.
+//  Copyright © 2019 monda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MagicThought.
+FOUNDATION_EXPORT double MagicThoughtVersionNumber;
+
+//! Project version string for MagicThought.
+FOUNDATION_EXPORT const unsigned char MagicThoughtVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MagicThought/PublicHeader.h>
+
+
